7c91e8cc5128b4b643afa2e44bb56e73aed2e4a8
gabriele <gabriele.dipinto@gmail.com>
2025-07-17 01:10:05 +0200

    added settings for render

--- tutorspace/tutorspace/settings.py
+++ tutorspace/tutorspace/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'tutorspace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
   'default': {
     'ENGINE':   'django.db.backends.postgresql',
     'NAME':     os.getenv('POSTGRES_DB',     'tutorspace_db'),
@@ -86,6 +87,14 @@ DATABASES = {
     'HOST':     os.getenv('DB_HOST',         'db'),
     'PORT':     '5432',
   }
+}'''
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
