cb6e9f4140c782240a9f04513263b30b9389c105
gabriele <gabriele.dipinto@gmail.com>
2025-07-17 01:30:10 +0200

    mod to settings.py

--- tutorspace/tutorspace/settings.py
+++ tutorspace/tutorspace/settings.py
@@ -94,7 +94,6 @@ WSGI_APPLICATION = 'tutorspace.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
         ssl_require=True
     )
 }
