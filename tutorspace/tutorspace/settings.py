a1dafe5b7a5b612ffab88ecf1397a5959153dbbc
gabriele <gabriele.dipinto@gmail.com>
2025-07-17 01:19:30 +0200

    update allowed hosts

--- tutorspace/tutorspace/settings.py
+++ tutorspace/tutorspace/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure--qwapoljdsy83#!!10#jt5ya*o!3q-v#$k2-o8eggde(zkwm2x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost',
+                '127.0.0.1',
+                'tutorspace.onrender.com',]
 
 
 # Application definition
